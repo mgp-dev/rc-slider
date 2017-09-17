f97c1f6f3513228a10f638204252042fc6f25fa8
geowarin <geowarin@gmail.com>
2017-09-17 17:08:49 +0200

    Simplify build

--- rollup.config.js
+++ rollup.config.js
@@ -2,11 +2,12 @@ import pkg from './package.json';
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import replace from 'rollup-plugin-replace';
+import typescript from 'rollup-plugin-typescript2';
 
 export default [
   // browser-friendly UMD build
   {
-    input: 'lib/index.js',
+    input: 'src/index.js',
     output: {
       file: pkg.browser,
       format: 'umd'
@@ -14,34 +15,26 @@ export default [
     name: 'RcSlider',
     external: ["react", "react-dom"],
     plugins: [
-      // typescript({
-      //   verbosity: 3,
-      //   clean: true,
-      //   check: true,
-      // }),
-      resolve(), // so Rollup can find `ms`
-      commonjs({
-        namedExports: {
-          'node_modules/my-lib/index.js': [ 'named' ]
-        }
-      }), // so Rollup can convert `ms` to an ES module
+      typescript({
+        include: [ "src/*.js+(|x)", "src/**/*.js+(|x)" ],
+      }),
+      resolve(),
+      commonjs(),
       replace({
         'process.env.NODE_ENV': JSON.stringify( 'production' )
       })
     ]
   },
-
-  // CommonJS (for Node) and ES module (for bundlers) build.
-  // (We could have three entries in the configuration array
-  // instead of two, but it's quicker to generate multiple
-  // builds from a single configuration where possible, using
-  // the `targets` option which can specify `dest` and `format`)
+  // commonjs
   // {
-  // 	entry: 'src/index.ts',
-  // 	external: ['ms'],
-  // 	targets: [
-  // 		{ dest: pkg.main, format: 'cjs' },
-  // 		{ dest: pkg.module, format: 'es' }
-  // 	]
-  // }
+  //   input: 'src/index.js',
+  //   output: [
+  //     { file: pkg.main, format: 'cjs' },
+  //   ],
+  //   plugins: [
+  //     typescript({
+  //       target: "es6"
+  //     })
+  //   ]
+  // },
 ];
