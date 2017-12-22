11371e132db460d7b74d76540f495b42b80e97cb
geowarin <geowarin@gmail.com>
2017-12-22 15:45:12 +0100

    Fix tests

--- tests/Slider.test.js
+++ tests/Slider.test.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import { render, mount } from 'enzyme';
 import { renderToJson } from 'enzyme-to-json';
-import keyCode from 'rc-util/lib/KeyCode';
+import keyCode from '../src/util/KeyCode';
 import Slider from '../src/Slider';
 
 describe('Slider', () => {
