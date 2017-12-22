9dfe7c3d9d24beaa90b606aa1a352de8e3f06b1b
geowarin <geowarin@gmail.com>
2017-12-22 16:46:02 +0100

    Add watch mode

--- rollup.config.js
+++ rollup.config.js
@@ -5,7 +5,7 @@ import replace from 'rollup-plugin-replace';
 import typescript from 'rollup-plugin-typescript2';
 import uglify from 'rollup-plugin-uglify';
 
-const env = process.env.NODE_ENV
+const env = process.env.NODE_ENV;
 const config = [
   // browser-friendly UMD build
   {
